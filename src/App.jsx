02881ebb507a3bbb11121cee565eee5582fd63c7
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-15 22:41:30 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+  
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white text-4xl">
+      Hello Portfolio!
+    </div>
+  );
+}
+
+export default App;
