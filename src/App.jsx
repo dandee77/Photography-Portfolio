4c6ad9cf5991b6122fe5973520d5985d402494f8
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-23 07:08:52 +0800

    added hero section, gradient, and beam background

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 import ScrollFloat from "./components/scroll_float";
+import GradientText from "./components/gradient_text";
+import Beams from "./components/beams";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -24,8 +26,39 @@ export default function App() {
   return (
     <div id="smooth-wrapper" ref={wrapperRef} style={{ height: "100vh" }}>
       <div id="smooth-content" ref={contentRef}>
-        <section id="home">
-          <h1>Dandee Design</h1>
+        <section id="home" className="bg-[#111111]">
+          <div style={{ width: "100%", height: "100%", position: "absolute" }}>
+            <Beams
+              beamWidth={2}
+              beamHeight={15}
+              beamNumber={12}
+              lightColor="#ffffff"
+              speed={2}
+              noiseIntensity={1.75}
+              scale={0.2}
+              rotation={0}
+            />
+          </div>
+          <h1 className="text-white text-7xl z-10 font-extrabold">
+            Turning ideas into{""}
+            <GradientText
+              colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+              animationSpeed={10}
+              showBorder={false}
+              className="custom-class"
+            >
+              creative
+            </GradientText>
+          </h1>
+          <h1 className="text-white text-7xl z-10 font-extrabold mb-8">
+            solutions
+          </h1>
+          <p className="text-gray-400 text-1xl z-10 font-normal">
+            Innovative web developer crafting
+          </p>
+          <p className="text-gray-400 text-1xl z-10 font-normal">
+            unique user experiences.
+          </p>
         </section>
 
         <section id="about">
